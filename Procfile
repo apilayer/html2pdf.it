e0d262def5c519d77ece7279bc787c77224a0e09
Allan Ebdrup <allan@878.dk>
2012-06-09 12:49:36 +0200

    First attempt at generating pdf from stdout

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./source/app.js
--- source/webservices/pdf.js
+++ source/webservices/pdf.js
@@ -1,6 +1,29 @@
+var spawn = require('child_process').spawn;
+var path = require("path");
+var fs = require("fs");
+var mime = require("mime");
+
+var pdfExecutable = "wkhtmltopdf-linux-amd64";
+
 module.exports = function(http){
 
 	http.get("/pdf", function(req, res, next){
-		res.json("we're up and running");
+		res.setHeader('Content-Type', mime.lookup(path))
+		var url = req.param("url");
+
+		var pdfProcess = spawn(path.join(__dirname, "/../../bin/", pdfExecutable), ["-", "--javascript-delay", 500, url]);
+
+		pdfProcess.stdout.on('data', function(data) {
+			res.write(data);
+		});
+
+		pdfProcess.stderr.on('data', function(data) {
+			console.log('stderr: ' + data);
+		});
+
+		pdfProcess.on('exit', function(code) {
+			console.log('pdf process exited with code ' + code);
+			res.end();
+		});
 	});
-}
+};
