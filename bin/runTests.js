c5c88d1f22480b67ceb3f1cae086a2ccb025c006
Allan Ebdrup <allan@878.dk>
2013-08-18 07:49:24 +0200

    update to express v3 and fix bug

--- bin/runTests.js
+++ bin/runTests.js
@@ -2,22 +2,14 @@
 var fs = require("fs");
 var path = require("path");
 
-//make writes to stdout and stderr sync, so the process does not exit before writing output.
-process.stdout.write = function (data){
-	fs.writeSync(1, data);
-};
-process.stderr.write = function (data){
-	fs.writeSync(2, data);
-};
-
-//runJsHint(["lib", "test", "bin"], function (err) {
-//	if (err) {
-//		console.error("Exiting because of jsHint errors");
-//		return process.exit(1);
-//	}
-//	console.log("No JSHint errors detected");
+runJsHint(["lib", "test", "bin"], function (err) {
+	if (err) {
+		console.error("Exiting because of jsHint errors");
+		return process.exit(1);
+	}
+	console.log("No JSHint errors detected");
 	return runMocha();
-//});
+});
 
 function runMocha() {
 	require('../node_modules/mocha/bin/_mocha');
