cb285beefcff81db3b882d495383c6614a635c7b
Allan Ebdrup <allan@muscula.com>
2013-08-18 07:25:14 +0200

    Update common.js

--- test/common.js
+++ test/common.js
@@ -3,7 +3,6 @@ var chai = require('chai');
 global.chai = chai;
 global.expect = chai.expect;
 global.request = require("request");
-global.requiremock = require("requiremock");
 global._ = require("underscore");
 var env = process.env.APP_ENV || "production";
 global.config = require("../config/" + env + ".app.config.js");
