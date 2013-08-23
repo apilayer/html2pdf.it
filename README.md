9228e4029bc808769a0b458479967c3ba78ccf23
Allan Ebdrup <aeb@e-conomic.com>
2013-08-23 16:16:06 +0200

    Add domain error handling and some output. Make it run on my Mac Clean up tests.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Using PhantomJS to generate pdfs, via a webservice. Runs using node.js.
 See it in action at: [html2pdf.it](http://www.html2pdf.it).
 
 Ready to fork and push to heroku (where I run it).
+
+Should work on both windows and linux.
+
+On Mac you need to
+```
+brew install phantomjs
+```
--- test/integration/pdf.spec.js
+++ test/integration/pdf.spec.js
@@ -1,16 +1,50 @@
-"use strict";
-var app = require("../../lib/app.js");
-describe('pdf service', function() {
+describe("setting up a test page", function () {
+	var app;
+	before(function () {
+		app = require("../../lib/app.js").app;
+		app.get("/test", function (req, res) {
+			res.send(200, '<!doctype html><html lang=en><head><meta charset=utf-8><title>test</title></head>' +
+				'<body><p>content</p></body></html>');
+		});
+	});
 
-	it('returns well formed pdf response', function(done) {
-		this.timeout(180000);
-		var pdfUrl = "http://www.html2pdf.it";
-		var url = "http://localhost:" + config.http.port +"/?url=" + encodeURIComponent(pdfUrl);
-		request(url, function(err, res, body) {
-			expect(res.statusCode).to.equal(200);
-			expect(res.headers["content-type"]).to.equal("application/pdf");
-			expect(body.substring(1, 4)).to.equal("PDF");
-			done();
+	describe('getting test page', function () {
+		var testUrl = "http://localhost:" + config.http.port + "/test";
+		before(function (done) {
+			return request(testUrl, function (err, resp) {
+				if (err) {
+					return done(err);
+				}
+				expect(resp, resp.body).to.have.property("statusCode", 200);
+				return done();
+			});
+		});
+
+		describe('getting a pdf of test page', function () {
+			this.timeout(18000);
+			var response;
+			before(function (done) {
+				var url = "http://localhost:" + config.http.port + "/?url=" + encodeURIComponent(testUrl);
+				return request(url, function (err, resp) {
+					if (err) {
+						return done(err);
+					}
+					response = resp;
+					return done();
+				});
+			});
+
+			it('should return statusCode 200', function () {
+				expect(response, response.body).to.have.property("statusCode", 200);
+			});
+
+			it('should return application/pdf as content-type', function () {
+				expect(response.headers).to.have.property("content-type", "application/pdf");
+			});
+
+			it('should return content that looks like binary PDF', function () {
+				expect(response.body.substring(1, 4)).to.equal("PDF");
+			});
 		});
 	});
 });
