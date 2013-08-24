e0329a0d4ad40769f51df61a6879ef8a955f68da
Allan Ebdrup <aeb@e-conomic.com>
2013-08-24 16:38:05 +0200

    support different orientations

--- test/integration/pdf.spec.js
+++ test/integration/pdf.spec.js
@@ -126,6 +126,59 @@ describe("setting up a test page", function () {
 			});
 		});
 
+		describe('getting a pdf of test page orientation landscape', function () {
+			this.timeout(18000);
+			var response;
+			before(function (done) {
+				var url = "http://localhost:" + config.http.port + "/?url=" + encodeURIComponent(testUrl) +
+					"&orientation=landscape";
+				return request(url, function (err, resp) {
+					if (err) {
+						return done(err);
+					}
+					response = resp;
+					return done();
+				});
+			});
+
+			it('should return statusCode 200', function () {
+				expect(response, response.body).to.have.property("statusCode", 200);
+			});
+
+			it('should return application/pdf as content-type', function () {
+				expect(response.headers).to.have.property("content-type", "application/pdf");
+			});
+
+			it('should return content that looks like binary PDF', function () {
+				expect(response.body.substring(1, 4)).to.equal("PDF");
+			});
+		});
+
+		describe('getting a pdf of test page invalid orientation', function () {
+			this.timeout(18000);
+			var response;
+			before(function (done) {
+				var url = "http://localhost:" + config.http.port + "/?url=" + encodeURIComponent(testUrl) + "&orientation=INVALID";
+				return request(url, function (err, resp) {
+					if (err) {
+						return done(err);
+					}
+					response = resp;
+					return done();
+				});
+			});
+
+			it('should return statusCode 400', function () {
+				expect(response, response.body).to.have.property("statusCode", 400);
+			});
+
+			it('should return correct error message', function () {
+				expect(response.body).to.equal(
+					"Invalid orientation, the following are supported: portrait, landscape"
+				);
+			});
+		});
+
 		describe('getting a pdf of a pdf of test page', function () {
 			this.timeout(18000);
 			var response;
