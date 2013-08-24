03e7359febbd948ef6b9381ac6a0e229351f7ddc
Allan Ebdrup <aeb@e-conomic.com>
2013-08-24 11:00:36 +0200

    make http:// optional in url

--- test/integration/pdf.spec.js
+++ test/integration/pdf.spec.js
@@ -9,9 +9,9 @@ describe("setting up a test page", function () {
 	});
 
 	describe('getting test page', function () {
-		var testUrl = "http://localhost:" + config.http.port + "/test";
+		var testUrl = "localhost:" + config.http.port + "/test";
 		before(function (done) {
-			return request(testUrl, function (err, resp) {
+			return request("http://" + testUrl, function (err, resp) {
 				if (err) {
 					return done(err);
 				}
@@ -21,6 +21,33 @@ describe("setting up a test page", function () {
 		});
 
 		describe('getting a pdf of test page', function () {
+			this.timeout(18000);
+			var response;
+			before(function (done) {
+				var url = "http://localhost:" + config.http.port + "/?url=" + encodeURIComponent("http://" + testUrl);
+				return request(url, function (err, resp) {
+					if (err) {
+						return done(err);
+					}
+					response = resp;
+					return done();
+				});
+			});
+
+			it('should return statusCode 200', function () {
+				expect(response, response.body).to.have.property("statusCode", 200);
+			});
+
+			it('should return application/pdf as content-type', function () {
+				expect(response.headers).to.have.property("content-type", "application/pdf");
+			});
+
+			it('should return content that looks like binary PDF', function () {
+				expect(response.body.substring(1, 4)).to.equal("PDF");
+			});
+		});
+
+		describe('getting a pdf of test page, without http://', function () {
 			this.timeout(18000);
 			var response;
 			before(function (done) {
